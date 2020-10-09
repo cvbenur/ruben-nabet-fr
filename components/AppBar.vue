2548c1f6ec5562d358afa2512dc0e77828f6c5c2
Ruben Nabet <rubenabet@gmail.com>
2020-10-09 15:06:54 +0200

    Set-up project

---
+++ components/AppBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app-bar
+    app
+    color="#000000"
+  >
+    <div>Bite</div>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({})
+export default class AppBar extends Vue {
+}
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,94 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    titleTemplate: '%s - ruben-nabet-fr',
+    title: 'ruben-nabet-fr',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Poppins&display=swap'
+      }
+    ]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify'
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    [
+      'nuxt-i18n',
+      {
+        locales: ['en', 'fr'],
+        defaultLocale: 'en',
+        vueI18n: {
+          fallbackLocale: 'en',
+          messages: {
+            en: {
+              greeting: 'Welcome'
+            },
+            fr: {
+              greeting: 'Bienvenue'
+            }
+          }
+        }
+      }
+    ]
+  ],
+
+  // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: '#141414',
+          accent: '#E66082',
+          secondary: '#733698',
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        },
+        light: {
+          primary: '#6699CC',
+          accent: '#E66082',
+          secondary: '#733698',
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+  }
+}
