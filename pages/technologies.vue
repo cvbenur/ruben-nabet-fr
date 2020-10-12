b5ad6a6e7b46744a75799ae8f8c2b2c07051cb67
Ruben Nabet <rubenabet@gmail.com>
2020-10-12 11:22:10 +0200

    Start work on technologies view

---
+++ pages/technologies.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Technologies</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({})
+export default class Technologies extends Vue {
+
+}
+</script>
