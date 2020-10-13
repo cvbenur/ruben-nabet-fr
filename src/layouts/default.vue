d9663ec535c541305fe8f44f04473dc7150c4d12
Ruben Nabet <rubenabet@gmail.com>
2020-10-13 19:48:50 +0200

    Integrate NotifSnackbar component

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -5,6 +5,11 @@
     <v-main>
       <v-container class="mt-10 mx-auto px-lg-10 px-2 px-sm-8">
         <nuxt />
+        <NotifSnackbar
+          ref="notifSnack"
+          :message="message"
+          :icon="icon"
+        />
       </v-container>
     </v-main>
 
@@ -13,10 +18,28 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component, Watch, Ref } from 'vue-property-decorator'
+import NotifSnackbar from '~/components/app/NotifSnackbar.vue'
 
 @Component({})
 export default class Default extends Vue {
+  private notif: boolean = false;
+
+  private message: string = '';
+  private icon: string = '';
+
+  @Watch('$vuetify.theme.dark')
+  themeChange (newVal: boolean) : void {
+    this.message = newVal ? 'Dark' : 'Light'
+    this.icon = newVal ? 'far fa-moon' : 'fas fa-sun'
+    this.turnOnNotification()
+  }
+
+  @Ref() notifSnack!: NotifSnackbar;
+
+  turnOnNotification () {
+    this.notifSnack.turnOn()
+  }
 }
 </script>
 
