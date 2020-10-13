82630f258cd73adca461dd855acebe3980d7aac3
Ruben Nabet <rubenabet@gmail.com>
2020-10-13 18:45:44 +0200

    Update TechTile component

--- src/components/technologies/TechTile.vue
+++ src/components/technologies/TechTile.vue
@@ -1,60 +1,58 @@
 <template>
-  <v-row
-    align="center"
-    justify="center"
-    class="text-center"
-  >
-    <v-col
-      cols="3"
-      sm="2"
+  <v-container class="fluid fill-height">
+    <v-row
+      align="center"
+      justify="center"
+      class="text-center"
     >
-      <v-row align="center" justify="center">
-        <div :class="`mx-0 shifted-bottom-${shift}`">
-          <v-card
-            width="120"
-            height="120"
-            :color="techno.tileColor"
-            rounded
-            hover
-            :href="`${techno.link}`"
-          >
-            <v-container class="fluid fill-height py-0">
-              <v-row
-                align="center"
-                justify="center"
-                class="pa-0"
-              >
-                <v-col
-                  cols="12"
-                  sm="12"
+      <v-col>
+        <v-row align="center" justify="center">
+          <div :class="`mx-0 shifted-bottom-${shift}`">
+            <v-card
+              width="120"
+              height="120"
+              :color="techno.tileColor"
+              rounded
+              hover
+              :href="`${techno.link}`"
+            >
+              <v-container class="fluid fill-height py-0">
+                <v-row
+                  align="center"
+                  justify="center"
                   class="pa-0"
                 >
-                  <v-img
-                    :src="require(`@/assets/svg/${techno.icon}`)"
-                    :alt="techno.title"
-                    aspect-ratio="1"
-                    :width="size"
-                    :height="size"
-                    fluid
-                    class="rounded-img"
-                  />
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-card>
-        </div>
-      </v-row>
+                  <v-col
+                    class="pa-0 d-flex justify-center"
+                  >
+                    <v-img
+                      :src="require(`@/assets/svg/${techno.icon}`)"
+                      :alt="techno.title"
+                      aspect-ratio="1"
+                      :width="size"
+                      :height="size"
+                      fluid
+                      contain
+                      class="rounded-img"
+                    />
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card>
+          </div>
+        </v-row>
 
-      <v-row
-        align="center"
-        justify="center"
-      >
-        <p class="pt-3 d-flex flex-nowrap">
-          {{ techno.title }}_
-        </p>
-      </v-row>
-    </v-col>
-  </v-row>
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <p class="pt-3 mb-0">
+            {{ techno.title }}_
+          </p>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
