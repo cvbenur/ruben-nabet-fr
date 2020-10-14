c7e3ca5265ae2cc1272c82fb5534dd9d1f716b7a
Ruben Nabet <rubenabet@gmail.com>
2020-10-14 09:53:26 +0200

    Update title parameters in config

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     titleTemplate: '%s | Ruben Nabet 😋',
-    title: 'Full-Stack Developer and Engineering Student',
+    title: '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
