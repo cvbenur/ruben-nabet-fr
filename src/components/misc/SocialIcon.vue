dfacd84097f25aefd1fc6b5b3a91ca5fbe06301f
Ruben Nabet <rubenabet@gmail.com>
2020-10-16 00:31:50 +0200

    Create SocialIcon component

---
+++ src/components/misc/SocialIcon.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-btn
+    color="#6699CC"
+    ripple
+    small
+    :href="`${link}`"
+    target="_blank"
+    fab
+    :elevation="0"
+  >
+    <v-icon color="white">
+      {{ icon }}
+    </v-icon>
+  </v-btn>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component({})
+export default class SocialIcon extends Vue {
+  @Prop({ required: true })
+  link!: string;
+
+  @Prop({ required: true })
+  icon!: string;
+}
+</script>
