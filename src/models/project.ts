44575f44638adc23c2a3c2693f2a1206ba2f1c72
Ruben Nabet <rubenabet@gmail.com>
2020-10-16 22:48:44 +0200

    Update project model

--- src/models/project.ts
+++ src/models/project.ts
@@ -1,9 +1,9 @@
 import { projects } from '@/assets/resources/projects.json'
 
 export class Project {
-  private title: string;
-  private link: string;
-  private repo: string;
+  title: string;
+  link: string;
+  repo: string;
   techList: Array<string>;
 
   constructor (title: string, link: string, repo: string, techList: Array<string>) {
