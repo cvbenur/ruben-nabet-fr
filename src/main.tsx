b389916c81dfccbd56d4eaed6ed0e22cf2cbb4ca
Ruben Nabet <rubenabet@gmail.com>
2025-10-05 21:25:25 +0200

    chore(setup): add i18n

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import "@middlewares/languages/init-i18next.ts";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
